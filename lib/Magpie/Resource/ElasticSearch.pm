0c787ffd048aa5f523eec68188c97454d0e8b626
Chris Prather <chris@prather.org>
2011-12-13 12:13:26 -0500

    unfinished first draft of an ElasticSearch resource

--- lib/Magpie/Resource/ElasticSearch.pm
+++ lib/Magpie/Resource/ElasticSearch.pm
@@ -1,9 +1,34 @@
-package Magpie-Resource-ElasticSearch;
+package Magpie -Resource-ElasticSearch;
 use Moose;
 use namespace::autoclean;
 
-# ABSTRACT: My New App!
+# ABSTRACT: An Elastic Search Resource for Magpie
 
+use Magpie::Constants;
+use ElasticSearch;
+
+extends 'Magpie::Resource';
+
+has elastic_search => (
+    isa     => 'ElasticSearch',
+    is      => 'ro',
+    lazy    => 1,
+    builder => '_build_elastic_search'
+);
+
+sub _build_elastic_search {
+    my $self = shift;
+    return ElasticSearch->new( servers => $self->servers );
+}
+
+sub search {
+    my ( $self, $index, $string ) = @_;
+    my $es = $self->elastic_search;
+    $es->search(
+        index  => $index,
+        queryb => $search_string,
+    );
+}
 
 __PACKAGE__->meta->make_immutable;
 1;
